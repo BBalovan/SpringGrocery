96ff86b526cb9ca269e11cca4bdbce2a923e2d4a
speti43 <speti43@gmail.com>
2014-06-14 23:50:32 +0200

    Lambda test

--- src/main/java/services/RedFruitService.java
+++ src/main/java/services/RedFruitService.java
@@ -1,13 +1,35 @@
 package services;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class RedFruitService implements FruitService {
 
 	@Override
 	public List<String> getFuits() {
-		return Arrays.asList("Apple", "Tomato");
+		return StrategyTest((fruitList) -> {
+			List<String> extendedList = new ArrayList<String>();
+			extendedList.add("Red fruit Additional");
+			extendedList.addAll(fruitList);
+			Collections.sort(extendedList);
+			return extendedList;
+		});
+	}
+
+	public List<String> getFuitsAnotherWay() {
+		return StrategyTest((fruitList) -> {
+			Collections.sort(fruitList);
+			return fruitList;
+		});
+	}
+
+	public List<String> StrategyTest(ListMethods strategy) {
+		List<String> fruits = Arrays.asList("Apple", "Tomato", "Currant",
+				"Cranberry");
+		return strategy.SortFruits(fruits);
 	}
 
 }
